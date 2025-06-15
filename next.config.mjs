ff11a966e58919c9e148da7001a16b65433ca1b6
akshxdevs <akshxdevs@gmail.com>
2025-06-15 16:04:10 +0530

    v1.1

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,21 @@
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
-
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-      plugins: [nodePolyfills(),],
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+      buffer: require.resolve("buffer/"),
+      assert: require.resolve("assert/"),
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      os: require.resolve("os-browserify/browser"),
+      url: require.resolve("url/"),
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
